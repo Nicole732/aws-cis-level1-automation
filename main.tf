4f017a236f57db02d685dda1108d44b320a786ac
nicole732 <dketchiiozo@gmail.com>
2025-05-03 14:23:56 -0400

    updated code

--- main.tf
+++ main.tf
@@ -4,18 +4,18 @@
 #CIS 1.1: Maintain current contact details#
 module "sns" {
   source      = "../../modules/sns_topic"
-  topic_name  = "cis-contact-check-topic"
+  topic_name  = "cis1.1-contact-check-topic"
   email       = var.alert_email
 }
 
 module "iam_role" {
   source     = "../../modules/iam_lambda_role"
-  role_name  = "cis-contact-check-role"
+  role_name  = "cis1.1-contact-check-role"
 }
 
 module "lambda" {
   source                    = "../../modules/lambda_function"
-  function_name             = "cis_contact_check"
+  function_name             = "cis1.1_contact_check"
   zip_file                  = "lambda_contact_check.zip"
   handler                   = "lambda_function.lambda_handler"
   lambda_execution_role_arn = module.iam_role.role_arn
@@ -26,7 +26,7 @@ module "lambda" {
 
 module "schedule" {
   source                = "../../modules/cloudwatch_schedule"
-  schedule_name         = "cis_contact_check_schedule"
+  schedule_name         = "cis1.1_contact_check_schedule"
   schedule_expression   = "rate(1 day)"
   lambda_function_arn   = module.lambda.this.arn
   lambda_function_name  = module.lambda.this.function_name
