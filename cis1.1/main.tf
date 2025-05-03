a6e369172c6e1c2cf36cdb155102b7087d492820
nicole732 <dketchiiozo@gmail.com>
2025-05-03 14:53:52 -0400

    resolved errors

--- cis1.1/main.tf
+++ cis1.1/main.tf
@@ -1,28 +1,29 @@
 
 module "sns" {
-  source      = "../../modules/sns_topic"
-  topic_name  = "cis-contact-check-topic"
+  source      = "../modules/sns_topic"
+  topic_name  = "cis11-contact-check-topic"
   email       = var.alert_email
 }
 
 module "iam_role" {
-  source     = "../../modules/iam_lambda_role"
-  role_name  = "cis-contact-check-role"
+  source     = "../modules/iam_lambda_role"
+  role_name  = "cis11-contact-check-role"
 }
 
 module "lambda" {
-  source                    = "../../modules/lambda_function"
-  function_name             = "cis_contact_check"
+  source                    = "../modules/lambda_function"
+  lambda_function_name      = "cis1_1_contact_check" #module.lambda.lambda_function_name
   zip_file                  = "lambda_contact_check.zip"
   handler                   = "lambda_function.lambda_handler"
-  lambda_execution_role_arn = module.iam_role.role_arn
+  lambda_exec_role_arn = module.iam_role.role_arn
+
   environment_variables     = {
     SNS_TOPIC_ARN = module.sns.topic_arn
   }
 }
 
 module "schedule" {
-  source                = "../../modules/cloudwatch_schedule"
+  source                = "../modules/cloudwatch_schedule"
   schedule_name         = "cis_contact_check_schedule"
   schedule_expression   = "rate(1 day)"
   lambda_function_arn   = module.lambda.this.arn
--- modules/sns_topic/main.tf
+++ modules/sns_topic/main.tf
@@ -1,3 +1,4 @@
+
 resource "aws_sns_topic" "this" {
   name = var.topic_name
 }
