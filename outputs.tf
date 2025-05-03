32204f21dc325e1d6a80de898d4c9800969dbef3
nicole732 <dketchiiozo@gmail.com>
2025-05-03 15:09:47 -0400

    updated files

--- outputs.tf
+++ outputs.tf
@@ -3,8 +3,4 @@ output "aws_region_output" {
   description = "The AWS region being used"
   value       = var.aws_region
 }
-#iam 1.1
-output "sns_topic_arn" {
-  description = "ARN of the current Contact Info Notification SNS Topic"
-  value       = aws_sns_topic.current_contact_details.arn
-}
+
