974c5d91957bd208a3615ede3ce73f0851d6153a
nicole732 <dketchiiozo@gmail.com>
2025-05-03 22:14:55 -0400

    updated cis 1.3 and 1.4 configurations

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -46,8 +46,8 @@ module "lambda" {
 }
 
 module "schedule" {
-  source               = "../../modules/cloudwatch_schedule"
-  schedule_name        = "cis1_1_contact_check_schedule"
+  source        = "../../modules/cloudwatch_schedule"
+  schedule_name = "cis1_1_contact_check_schedule"
   #"rate(1 day)" #"rate(5 minutes) for testing
   schedule_expression  = "rate(5 minutes)"
   lambda_function_arn  = module.lambda.lambda_function_arn
@@ -59,3 +59,19 @@ module "schedule" {
 # AWS Config managed rule: iam-root-acces-key-check
 
 
+module "cis_1_3_root_key_check" {
+  source            = "../modules/aws_config_rule"
+  rule_name         = "cis-1-3-root-access-key-check"
+  topic_name        = "cis13-croot-access-key-alerts"
+  description       = "CIS 1.3: Ensure no root user account access key exists"
+  source_identifier = "IAM_ROOT_ACCESS_KEY_CHECK"
+}
+
+module "cis_1_4_root_mfa_check" {
+  source            = "../modules/aws_config_rule"
+  rule_name         = "cis-1-4-root-mfa-check"
+  topic_name        = "cis14-root-mfa-alerts"
+  description       = "CIS 1.4: Ensure MFA is enabled for the root user"
+  source_identifier = "ROOT_ACCOUNT_MFA_ENABLED" #root-account-mfa-enabled
+
+}
--- env/dev/variables.tf
+++ env/dev/variables.tf
@@ -22,3 +22,9 @@ variable "alert_email" {
   description = "The email address to notify about maintaining AWS contact details"
   type        = string
 }
+
+#CIS 1.3
+variable "config_role_arn" {
+  description = "The ARN of the IAM role used by AWS Config"
+  type        = string
+}
