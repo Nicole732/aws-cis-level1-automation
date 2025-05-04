f8fb673fa733a9c6d0ae96276af2b95549808d7e
nicole732 <dketchiiozo@gmail.com>
2025-05-03 23:06:34 -0400

    resolved errors

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -58,19 +58,16 @@ module "schedule" {
 # AWS Config continuously monitor control and returns Compliant/Non Compliant Status
 # AWS Config managed rule: iam-root-acces-key-check
 
-
 module "cis_1_3_root_key_check" {
-  source            = "../modules/aws_config_rule"
+  source            = "../../modules/aws_config"
   rule_name         = "cis-1-3-root-access-key-check"
-  topic_name        = "cis13-croot-access-key-alerts"
   description       = "CIS 1.3: Ensure no root user account access key exists"
   source_identifier = "IAM_ROOT_ACCESS_KEY_CHECK"
 }
 
 module "cis_1_4_root_mfa_check" {
-  source            = "../modules/aws_config_rule"
+  source            = "../../modules/aws_config"
   rule_name         = "cis-1-4-root-mfa-check"
-  topic_name        = "cis14-root-mfa-alerts"
   description       = "CIS 1.4: Ensure MFA is enabled for the root user"
   source_identifier = "ROOT_ACCOUNT_MFA_ENABLED" #root-account-mfa-enabled
 
